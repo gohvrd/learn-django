b5c230328d4a85a8b5ada868097fd565d4eb1b0d
gohvrd <artemkuzmenko97@gmail.com>
2021-05-08 00:39:47 +0300

    first app

---
+++ articles/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+
+class Article(models.Model):
+    article_title = models.CharField('название статьи', max_length=200)
+    article_text = models.TextField('текст статьи')
+    pub_date = models.DateTimeField('дата публикации')\
+
+    def __str__(self):
+        return self.article_title
+
+    def was_publish_recently(self):
+        return self.pub_date >= (timezone.now() - datetime.timedelta(days=7))
+
+    class Meta:
+        verbose_name = 'Art'
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete = models.CASCADE)
+    author_name = models.CharField('имя автора', max_length=50)
+    comment_text = models.CharField('текст комментария', max_length=200)
+
+    def __str__(self):
+        return self.author_name
+
+    class Meta:
+        verbose_name = 'Comm'
